7bb90a7ea521ee672a3c694e650f46daa2b51580
avinashkt04 <kumaravinash12915@gmail.com>
2024-12-18 23:47:22 +0530

    43_Queue_Ques

--- 43_Queue_Ques.cpp
+++ 43_Queue_Ques.cpp
@@ -63,10 +63,148 @@ void interLeaveQueue(queue<int> &q) {
         q.push(q.front());
         q.pop();
     }
-
-
-
 }
+
+// k queue in an array
+
+class kQueue {
+    public:
+        int n;
+        int k;
+        int *front;
+        int *rear;
+        int *arr;
+        int freespot;
+        int *next;
+
+    public:
+        kQueue(int n, int k) {
+            this -> n = n;
+            this -> k = k;
+            front = new int[k];
+            rear = new int[k];
+
+            for(int i=0; i<k; i++) {
+                front[i] = -1;
+                rear[i] = -1;
+            }
+
+            next = new int[n];
+            for(int i=0; i<n; i++){
+                next[i] = i+1;
+            }
+            next[n-1] = -1;
+
+            arr = new int[n];
+            freespot = 0;
+        }
+
+        void enqueue(int data, int qn) {
+            // overflow
+            if(freespot == -1) {
+                cout << "No empty space is present" << endl;
+            }
+
+            // find first free index
+            int index = freespot;
+
+            // update freespot
+            freespot = next[index];
+
+            // check whether first element
+            if(front[qn-1] == -1) {
+                front[qn-1] = index;
+            } else {
+                // link new element to the prev element
+                next[rear[qn-1]] = index;
+            }
+
+            // update next
+            next[index] = -1;
+
+            // update rear
+            rear[qn-1] = index;
+
+            // push element
+            arr[index] = data;
+        }
+
+        int deque(int qn) {
+            // underflow
+            if(front[qn-1] == -1){
+                cout << "Queue underflow" << endl;
+                return -1;
+            }
+
+            // find index to pop
+            int index = front[qn-1];
+
+            // front ko aage badhao
+            front[qn-1] = next[index];
+
+            // freeSlots ko manage karo
+            next[index] = freespot;
+            freespot = index;
+
+            return arr[index];
+        }
+};
+
+// sum of max and min
+int solve(int *arr, int n, int k) {
+
+    deque<int> maxi(k);
+    deque<int> mini(k);
+
+    //Addition of first k size window
+
+    for(int i=0; i<k; i++) {
+
+        while(!maxi.empty() && arr[maxi.back()] <= arr[i])
+            maxi.pop_back();
+
+        while(!mini.empty() && arr[mini.back()] >= arr[i])
+            mini.pop_back();
+
+        maxi.push_back(i);
+        mini.push_back(i);
+    }
+    int ans = 0;
+
+    ans += arr[maxi.front()] + arr[mini.front()];
+
+    //remaining windows ko process karlo
+    for(int i=k; i<n; i++) {
+
+        
+
+        //next window
+
+        //removal
+        while(!maxi.empty() && i - maxi.front() >=k) {
+            maxi.pop_front();
+        }
+
+        while(!mini.empty() && i - mini.front() >=k) {
+            mini.pop_front();
+        }
+
+        //addition
+
+        while(!maxi.empty() && arr[maxi.back()] <= arr[i])
+            maxi.pop_back();
+
+        while(!mini.empty() && arr[mini.back()] >= arr[i])
+            mini.pop_back();
+
+        maxi.push_back(i);
+        mini.push_back(i);    
+
+        ans += arr[maxi.front()] + arr[mini.front()];
+    }
+    return ans;
+}
+
 
 int main() {
     queue<int> q;
@@ -86,5 +224,23 @@ int main() {
         cout << q.front() << " ";
         q.pop();
     }
+    cout << endl;
+
+    kQueue kq(10, 3);
+
+    kq.enqueue(10, 1);
+    kq.enqueue(15,1);
+    kq.enqueue(20, 2);
+    kq.enqueue(25, 1);
+
+    cout << kq.deque(1) << endl;
+    cout << kq.deque(2) << endl;
+    cout << kq.deque(1) << endl;
+    cout << kq.deque(1) << endl;
+
+    int arr[7] = {2, 5, -1, 7, -3, -1, -2};
+    int k = 4;
+    cout << solve(arr, 7, k) << endl;
+
     return 0;
 }
